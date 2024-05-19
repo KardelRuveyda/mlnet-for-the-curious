c918c211dfbe812511e97628ff3077cba432c499
Ruveyda Cetin <ruveyda.cetin@d-teknoloji.com.tr>
2024-05-19 15:56:09 +0300

    birtakım değişiklikler

--- MLPredictionConsoleApp/obj/Debug/net8.0/MLPredictionConsoleApp.AssemblyInfo.cs
+++ MLPredictionConsoleApp/obj/Debug/net8.0/MLPredictionConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLPredictionConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f5f1c4faf74c226ed89da2330d95b0dd15fd86")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLPredictionConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLPredictionConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
